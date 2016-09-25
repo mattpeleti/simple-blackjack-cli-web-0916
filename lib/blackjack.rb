527dcdb875406d346778ddf59cd5c64da1f58003
Matt Peleti <mattpeleti@gmail.com>
2016-09-25 05:18:59 +0000

    only took 20 tries

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,67 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rnum=Random.new
+  rnum.rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(totalnum)
+  puts "Your cards add up to #{totalnum}"
+  totalnum
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  reply= gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(totalnum)
+  puts "Sorry, you hit #{totalnum}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card1 = deal_card
+  card2 = deal_card
+  totalnum = card1 + card2
+  display_card_total(totalnum)
+  totalnum
 end
 
-def hit?
-  # code hit? here
+def hit?(totalnum)
+  prompt_user
+  reply = get_user_input
+  if reply == "s"
+
+  elsif reply == "h"
+    totalnum = deal_card + totalnum
+  else
+    invalid_command
+    prompt_user
+  end
+  totalnum
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
+
 def runner
-  # code runner here
+  #until i die
+    welcome
+    totalnum=initial_round
+    until totalnum>21 do
+      totalnum=hit?(totalnum)
+      display_card_total(totalnum)
+    end
+    end_game(totalnum)
 end
-    
